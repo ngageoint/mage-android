9dd2e7dcdaabc6c819edba4a3dbd7695f16ffcdc
Dan Barela <daniel.barela@gmail.com>
2014-03-14 13:28:52 -0600

    observation view/edit updates

--- src/mil/nga/giat/mage/observation/ObservationEditActivity.java
+++ src/mil/nga/giat/mage/observation/ObservationEditActivity.java
@@ -1,5 +1,10 @@
 package mil.nga.giat.mage.observation;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Properties;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -7,14 +12,24 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import mil.nga.giat.mage.R;
-import mil.nga.giat.mage.preferences.PublicPreferencesActivity;
-import android.content.Intent;
+import mil.nga.giat.mage.sdk.database.orm.observation.Geometry;
+import mil.nga.giat.mage.sdk.database.orm.observation.GeometryType;
+import mil.nga.giat.mage.sdk.database.orm.observation.Observation;
+import mil.nga.giat.mage.sdk.database.orm.observation.ObservationHelper;
+import mil.nga.giat.mage.sdk.database.orm.observation.Property;
+import mil.nga.giat.mage.sdk.database.orm.observation.State;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ObservationEditActivity extends FragmentActivity {
+	
+	Observation o;
+	Date d;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,14 +40,15 @@ public class ObservationEditActivity extends FragmentActivity {
 		GoogleMap map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
 		
 		// TODO debugging location
-		LatLng sydney = new LatLng(-33.867, 151.206);
+		LatLng location = new LatLng(39.7, -104.0);
 		
-		map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
 		
-		map.addMarker(new MarkerOptions()
-        .title("Sydney")
-        .snippet("The most populous city in Australia.")
-        .position(sydney));
+		map.addMarker(new MarkerOptions().position(location));
+		((TextView)findViewById(R.id.location)).setText(location.latitude + ", " + location.longitude);
+		d = new Date();
+		((TextView)findViewById(R.id.date)).setText(d.toString());
+		
 	}
 	
 	@Override
@@ -49,6 +65,30 @@ public class ObservationEditActivity extends FragmentActivity {
 	
 		case R.id.observation_save:
 			System.out.println("SAVE");
+			
+			double lat = 39.7;
+			double lon = -104.0;
+			
+			
+			//o.setGeometry(new Geometry(lat, lon), new GeometryType("point"));
+			o = new Observation();
+			o.setState(new State("active"));
+			o.setGeometry(new Geometry("[" + lat + "," + lon + "]", new GeometryType("point")));
+			Collection<Property> properties = new ArrayList<Property>();
+			properties.add(new Property("OBSERVATION_DATE", String.valueOf(d.getTime())));
+			properties.add(new Property("TYPE", (String)((Spinner)findViewById(R.id.type_spinner)).getSelectedItem()));
+			properties.add(new Property("LEVEL", ((EditText)findViewById(R.id.level)).getText().toString()));
+			properties.add(new Property("TEAM", ((EditText)findViewById(R.id.team)).getText().toString()));
+			properties.add(new Property("DESCRIPTION", ((EditText)findViewById(R.id.description)).getText().toString()));
+			
+			ObservationHelper oh = ObservationHelper.getInstance(getApplicationContext());
+			try {
+				Observation newObs = oh.createObservation(o);
+				System.out.println(newObs.getPk_id());
+			} catch (Exception e) {
+				
+			}
+			
 			break;
 		}
 
