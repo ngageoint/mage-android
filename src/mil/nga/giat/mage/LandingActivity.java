0e3596786a87987507d064dbd9cd2c89159a3ee1
William Newman <newmanw10@gmail.com>
2014-03-14 13:29:35 -0600

    Map MapActivity is now a MapFragment extending from Fragment. The new MapFragment is now part of the LandingActivity which is a TabActivity. Created place holder class for the news feed fragment.  Just displays news feed text.

--- src/mil/nga/giat/mage/LandingActivity.java
+++ src/mil/nga/giat/mage/LandingActivity.java
@@ -2,6 +2,7 @@
 
 import java.util.Locale;
 
+import mil.nga.giat.mage.newsfeed.NewsFeedFragment;
 import mil.nga.giat.mage.observation.ObservationViewActivity;
 import mil.nga.giat.mage.preferences.PublicPreferencesActivity;
 import mil.nga.giat.mage.sdk.location.LocationService;
@@ -14,12 +15,8 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * FIXME: Currently a mock of what a landing page might look like. Could be
@@ -254,13 +251,21 @@ public class LandingActivity extends FragmentActivity implements ActionBar.TabLi
 
 		@Override
 		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a DummySectionFragment (defined as a static inner class
-			// below) with the page number as its lone argument.
-			Fragment fragment = new DummySectionFragment();
-			Bundle args = new Bundle();
-			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-			fragment.setArguments(args);
+			Fragment fragment = null;
+			switch (position) {
+				case 0: {
+					fragment = new MapFragment();
+					break;
+				}
+				case 1: {
+					fragment = new NewsFeedFragment();
+					break;
+				}
+				default: {
+					// TODO not sure what to do here, if anything (fix your code)
+				}
+			}
+			
 			return fragment;
 		}
 
@@ -281,31 +286,4 @@ public class LandingActivity extends FragmentActivity implements ActionBar.TabLi
 			return null;
 		}
 	}
-
-	/**
-	 * A dummy fragment representing a section of the app, but that simply
-	 * displays dummy text.
-	 */
-	public static class DummySectionFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		public static final String ARG_SECTION_NUMBER = "section_number";
-
-		public DummySectionFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main_dummy, container, false);
-
-			// LocationService locationService = new
-			// LocationService(container.getContext());
-			TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
-			dummyTextView.setText("Viewing " + Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-			return rootView;
-		}
-	}
-
 }
