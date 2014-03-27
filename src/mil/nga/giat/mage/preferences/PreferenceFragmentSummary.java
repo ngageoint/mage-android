d52bc0a8620dd0342ca756f9f7fa9019ca4e5c17
William Newman <newmanw10@gmail.com>
2014-03-27 11:47:47 -0600

    New List Preference Removed compass, tilt, and rotate gestures from the main map. Put in new list preferences with values

--- src/mil/nga/giat/mage/preferences/PreferenceFragmentSummary.java
+++ src/mil/nga/giat/mage/preferences/PreferenceFragmentSummary.java
@@ -2,6 +2,7 @@ package mil.nga.giat.mage.preferences;
 
 import java.util.Iterator;
 
+import mil.nga.giat.mage.sdk.preferences.ListValuePreference;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.preference.EditTextPreference;
@@ -13,55 +14,55 @@ import android.preference.PreferenceFragment;
 
 public class PreferenceFragmentSummary extends PreferenceFragment implements OnSharedPreferenceChangeListener {
 
-	public void setSummary(Preference p) {
-		if (p instanceof PreferenceCategory) {
-			PreferenceCategory pCat = (PreferenceCategory) p;
-			for (int i = 0; i < pCat.getPreferenceCount(); i++) {
-				setSummary(pCat.getPreference(i));
-			}
-		} else if (p instanceof ListPreference) {
-			ListPreference pref = (ListPreference) p;
-			p.setSummary(pref.getEntry());
-		} else if (p instanceof EditTextPreference) {
-			EditTextPreference pref = (EditTextPreference) p;
-			p.setSummary(pref.getText());
-		} else if (p instanceof MultiSelectListPreference) {
-			MultiSelectListPreference pref = (MultiSelectListPreference) p;
-			CharSequence[] entries = pref.getEntries();
-			StringBuffer summary = new StringBuffer();
+    public void setSummary(Preference preference) {
+        if (preference instanceof PreferenceCategory) {
+            PreferenceCategory c = (PreferenceCategory) preference;
+            for (int i = 0; i < c.getPreferenceCount(); i++) {
+                setSummary(c.getPreference(i));
+            }
+        } else if (preference instanceof ListValuePreference) {
+            ListValuePreference p = (ListValuePreference) preference;
+            p.setListValue(p.getEntry());
+        } else if (preference instanceof ListPreference) {
+            ListPreference p = (ListPreference) preference;
+            p.setSummary(p.getEntry());
+        } else if (preference instanceof EditTextPreference) {
+            EditTextPreference p = (EditTextPreference) preference;
+            p.setSummary(p.getText());
+        } else if (preference instanceof MultiSelectListPreference) {
+            MultiSelectListPreference p = (MultiSelectListPreference) preference;
+            CharSequence[] entries = p.getEntries();
+            StringBuffer summary = new StringBuffer();
 
-			Iterator<String> iterator = pref.getValues().iterator();
-			while (iterator.hasNext()) {
-				String value = iterator.next();
-				summary.append(entries[pref.findIndexOfValue(value)]);
+            Iterator<String> iterator = p.getValues().iterator();
+            while (iterator.hasNext()) {
+                String value = iterator.next();
+                summary.append(entries[p.findIndexOfValue(value)]);
 
-				if (iterator.hasNext())
-					summary.append("\n");
-			}
-			p.setSummary(summary);
-		}
-		
-//		else {
-//			p.setSummary(getPreferenceScreen().getSharedPreferences().getString(p.getKey(), ""));
-//		}
-	}
-	
-	@Override
-	public void onResume() {
-		super.onResume();
-		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-	}
+                if (iterator.hasNext())
+                    summary.append("\n");
+            }
+            p.setSummary(summary);
+        }
+    }
 
-	@Override
-	public void onPause() {
-		super.onPause();
-		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
-	}
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
 
-	@Override
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-		// TODO: android HACK as MultiSelectListPreferences don't fire updates as they should
-		Preference p = findPreference(key.split(DialogPreference.MultiSelectListPreferenceKey)[0]);
-		setSummary(p);
-	}
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        // TODO: android HACK as MultiSelectListPreferences don't fire updates
+        // as they should
+        Preference p = findPreference(key.split(DialogPreference.MultiSelectListPreferenceKey)[0]);
+        setSummary(p);
+    }
 }
