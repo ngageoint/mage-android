0ae841157cc5a07f6e89f7e6ad04cbcd3fab6fff
Scott Wiedemann <lemmingapex@gmail.com>
2014-05-22 21:10:38 -0600

    fixing timestamp bug.

--- src/mil/nga/giat/mage/observation/ObservationEditActivity.java
+++ src/mil/nga/giat/mage/observation/ObservationEditActivity.java
@@ -418,6 +418,9 @@ public class ObservationEditActivity extends Activity {
 
 			Map<String, ObservationProperty> propertyMap = LayoutBaker.populateMapFromLayout((LinearLayout) findViewById(R.id.form));
 			
+			// set the correct iso8601 date!
+			propertyMap.put("timestamp", new ObservationProperty("timestamp", DateUtility.getISO8601().format(date)));
+			
 			// Add properties that weren't part of the layout
 			propertyMap.put("accuracy", new ObservationProperty("accuracy", Float.toString(l.getAccuracy())));
 			String provider = l.getProvider();
