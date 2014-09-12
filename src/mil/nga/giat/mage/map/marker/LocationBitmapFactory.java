3335ce2bf624839b0c2b758230ee8921b65dfb87
Dan Barela <daniel.barela@gmail.com>
2014-09-12 10:16:54 -0600

    removed a bunch of old commented out code in LocationBitmapFactory

--- src/mil/nga/giat/mage/map/marker/LocationBitmapFactory.java
+++ src/mil/nga/giat/mage/map/marker/LocationBitmapFactory.java
@@ -1,15 +1,10 @@
 package mil.nga.giat.mage.map.marker;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 
 import mil.nga.giat.mage.sdk.datastore.location.Location;
 import mil.nga.giat.mage.sdk.datastore.user.User;
 import mil.nga.giat.mage.sdk.utils.MediaUtility;
-
-import org.apache.commons.lang3.StringUtils;
-
 import android.animation.ArgbEvaluator;
 import android.annotation.TargetApi;
 import android.content.Context;
@@ -32,20 +27,9 @@ public class LocationBitmapFactory {
 
 	private static Long upperBoundTimeInSeconds = 1800L;
 	
-//	private static ArrayList<DownloadImageTask> downloadTasks = new ArrayList<DownloadImageTask>();
-
 	// TODO : SCOTT UFM
 	private static final Integer[] colorGradient = { 0xff0000ff, 0xffffff00 ,0xffffa500 };
 	
-	// blue, yellow, orange
-	/*private static final Integer[] colorGradient = { 0xff0000ff, 0xff0000ff, 0xff0000ff, 0xff0807f8, 0xff0f10ef, 0xff1817e7, 0xff1f1fdf, 0xff2728d7, 0xff3030d0, 0xff3737c8, 0xff3f40bf, 0xff4847b7, 0xff4f4fb0, 0xff5757a7, 0xff5f5fa0, 0xff676797, 0xff706f8f, 0xff777787, 0xff7f7f7f, 0xff878878,
-			0xff8f9070, 0xff989767, 0xff9f9f60, 0xffa7a758, 0xffafb050, 0xffb8b848, 0xffbfbf40, 0xffc7c738, 0xffcfcf30, 0xffd8d828, 0xffdfe020, 0xffe7e718, 0xffefef10, 0xfff7f708, 0xffffff00, 0xffffff00, 0xfffffc00, 0xfffffa00, 0xfffff600, 0xfffff400, 0xfffff100, 0xffffee00, 0xffffeb00, 0xffffe800,
-			0xffffe600, 0xffffe300, 0xffffe100, 0xffffdd00, 0xffffdb00, 0xffffd700, 0xffffd500, 0xffffd200, 0xffffcf00, 0xffffcd00, 0xffffca00, 0xffffc700, 0xffffc400, 0xffffc100, 0xffffbf00, 0xffffbc00, 0xffffb900, 0xffffb600, 0xffffb300, 0xffffb000, 0xffffad00, 0xffffaa00, 0xffffa500 };*/
-	// hsv spectrum
-	private static final Integer[] colorGradient1 = { 0xff0000ff, 0xff0a00ff, 0xff1500ff, 0xff1f00ff, 0xff2a00ff, 0xff3500ff, 0xff3f00ff, 0xff4a00ff, 0xff5400ff, 0xff5f00ff, 0xff6900ff, 0xff7400ff, 0xff7e00ff, 0xff8900ff, 0xff9300ff, 0xff9e00ff, 0xffa800ff, 0xffb300ff, 0xffbe00ff, 0xffc800ff,
-			0xffd300ff, 0xffdd00ff, 0xffe800ff, 0xfff200ff, 0xfffc00ff, 0xffff00f6, 0xffff00ec, 0xffff00e1, 0xffff00d7, 0xffff00cc, 0xffff00c2, 0xffff00b7, 0xffff00ad, 0xffff00a2, 0xffff0098, 0xffff008d, 0xffff0083, 0xffff0078, 0xffff006d, 0xffff0063, 0xffff0058, 0xffff004e, 0xffff0044, 0xffff0039,
-			0xffff002e, 0xffff0023, 0xffff0019, 0xffff000e, 0xffff0004, 0xffff0700, 0xffff1100, 0xffff1b00, 0xffff2700, 0xffff3100, 0xffff3c00, 0xffff4600, 0xffff5100, 0xffff5b00, 0xffff6600, 0xffff7000, 0xffff7b00, 0xffff8500, 0xffff9000, 0xffff9a00, 0xffffa500 };
-
 	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
 	public static Bitmap bitmap(Context context, Location location, User user) {
 		Bitmap finalBitmap = null;
@@ -53,34 +37,13 @@ public class LocationBitmapFactory {
 		Log.d("LocationBitmapFactory", "Drawing the bitmap for user " + user.getUsername());
 		if (user.getLocalIconPath() != null) {
 			finalBitmap = combineIconAndDot(dotBitmap.copy(Bitmap.Config.ARGB_8888, true), BitmapFactory.decodeFile(user.getLocalIconPath()));
-//			m.setIcon(BitmapDescriptorFactory.fromBitmap(combined));
-		//} else if (user.getIconUrl() != null) {
-			// if there is supposed to be an icon but we don't have it, go get it
-//			if (ConnectivityUtility.isOnline(context)) {
-//				String token = PreferenceHelper.getInstance(context).getValue(R.string.tokenKey);
-//				DownloadImageTask task = new DownloadImageTask(dotBitmap.copy(Bitmap.Config.ARGB_8888, true), m, user, context);
-//				downloadTasks.add(task);
-//				task.execute(user.getIconUrl() + "?access_token=" + token);
-//				m.setIcon(BitmapDescriptorFactory.fromBitmap(dotBitmap));
-//			}
 		} else {
 			finalBitmap = dotBitmap;
-//			m.setIcon(BitmapDescriptorFactory.fromBitmap(finalBitmap));
 		}
 
 
 		return finalBitmap;
 	}
-	
-//	public static void cancelDownloadTasks() {
-//		Log.d("LocationBitmapFactory", "Cancelling " + downloadTasks.size() + " tasks");
-//		for (DownloadImageTask task : downloadTasks) {
-//			task.cancel(true);
-//			task = null;
-//		}
-//		downloadTasks.clear();
-//		Log.d("LocationBitmapFactory", "Cleared all of the download tasks");
-//	}
 	
 	private static Bitmap combineIconAndDot(Bitmap dot, Bitmap icon) {
 		Bitmap combined = Bitmap.createBitmap(96, 127, Config.ARGB_8888);
@@ -94,61 +57,6 @@ public class LocationBitmapFactory {
 		
 		return combined;
 	}
-	
-//	private static class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-//	    Marker marker;
-//	    Bitmap dotBitmap;
-//	    User user;
-//	    Context context;
-//
-//	    public DownloadImageTask(Bitmap dotBitmap, Marker m, User user, Context context) {
-//	        this.marker = m;
-//	        this.dotBitmap = dotBitmap;
-//	        this.user = user;
-//	        this.context = context;
-//	    }
-//
-//	    protected Bitmap doInBackground(String... urls) {
-//	        String urldisplay = urls[0];
-//	        Bitmap icon = null;
-//	        try {
-//	        InputStream in = new java.net.URL(urldisplay).openStream();
-//            icon = BitmapFactory.decodeStream(in);
-//	            
-//	        } catch (Exception e) {
-//	            Log.e("Error", e.getMessage());
-//	            e.printStackTrace();
-//	        }
-//	        return icon;
-//	    }
-//
-//	    protected void onPostExecute(Bitmap icon) {
-//	    	
-//	    	FileOutputStream out = null;
-//    		try {
-//    			String localPath = MediaUtility.getUserIconDirectory() + "/" + user.getId();
-//    		    out = new FileOutputStream(localPath);
-//    		    icon.compress(Bitmap.CompressFormat.PNG, 90, out);
-//    		    user.setLocalIconPath(localPath);
-//    		    UserHelper.getInstance(context).update(user);
-//    		} catch (Exception e) {
-//    		    e.printStackTrace();
-//    		} finally {
-//    		    try {
-//    		        if (out != null) {
-//    		            out.close();
-//    		        }
-//    		    } catch (IOException e) {
-//    		        e.printStackTrace();
-//    		    }
-//    		}
-//	    	
-//	    	Bitmap combined = combineIconAndDot(dotBitmap.copy(Bitmap.Config.ARGB_8888, true), icon.copy(Bitmap.Config.ARGB_8888, true));
-//	    	if (marker != null && combined != null && !this.isCancelled()) {
-//				marker.setIcon(BitmapDescriptorFactory.fromBitmap(combined));
-//			}
-//	    }
-//	}
 
 	public static BitmapDescriptor bitmapDescriptor(Context context, Location location, User user) {
 		Bitmap bitmap = bitmap(context, location, user);
@@ -217,30 +125,4 @@ public class LocationBitmapFactory {
 		return dotBitmap;
 	}
 
-	/**
-	 * @deprecated No long using hard coded assets.
-	 * 
-	 * @param location
-	 * @return
-	 */
-	private static String getAsset(Location location) {
-		if (location == null) {
-			return null;
-		}
-
-		Collection<String> paths = new ArrayList<String>();
-		paths.add("people");
-
-		Long interval = System.currentTimeMillis() - location.getTimestamp().getTime();
-		if (interval < 600000) { // 10 minutes
-			paths.add("low");
-		} else if (interval < 1800000) { // 30 minutes
-			paths.add("medium");
-		} else {
-			paths.add("high");
-		}
-
-		paths.add("person");
-		return StringUtils.join(paths, "/") + ".png";
-	}
 }
