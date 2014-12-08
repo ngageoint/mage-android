ed35a21725cc2e10c3dfa7232bf0ee39eedf0067
William Newman <newmanw10@gmail.com>
2014-12-08 13:50:37 -0700

    Cleaned up notifcation code

--- mage/src/main/java/mil/nga/giat/mage/MAGE.java
+++ mage/src/main/java/mil/nga/giat/mage/MAGE.java
@@ -140,25 +140,17 @@ public class MAGE extends MultiDexApplication implements IUserEventListener {
 		getLogoutPendingIntent().cancel();
 		boolean tokenExpired = UserUtility.getInstance(getApplicationContext()).isTokenExpired();
 
+        String notificationMsg = tokenExpired ? "Your token has expired, please tap to login." : "You are logged in. Slide down to logout.";
 		NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.ic_launcher)
                 .setContentTitle("MAGE")
                 .setOngoing(true)
 				.setPriority(NotificationCompat.PRIORITY_MAX)
+                .setContentText(notificationMsg)
                 .addAction(R.drawable.ic_power_off_white, "Logout", getLogoutPendingIntent());
 
         NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
-        bigTextStyle.setBigContentTitle("MAGE");
-
-        if (tokenExpired) {
-            String msg = "Your token has expired, please tap to login.";
-            bigTextStyle.bigText(msg);
-            builder.setContentText(msg);
-        } else {
-            String msg = "You are logged in. Slide down to logout.";
-            bigTextStyle.bigText(msg);
-            builder.setContentText(msg);
-        }
+        bigTextStyle.setBigContentTitle("MAGE").bigText(notificationMsg);
 
 		builder.setStyle(bigTextStyle);
 
