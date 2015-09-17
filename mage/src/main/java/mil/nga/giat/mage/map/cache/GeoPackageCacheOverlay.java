376c7958e28b932092b06e4dec5b6ba924247192
Brian Osborn <users.noreply.github.com>
2015-09-17 15:32:36 -0600

    GeoPackage updates

---
+++ mage/src/main/java/mil/nga/giat/mage/map/cache/GeoPackageCacheOverlay.java
@@ -0,0 +1,44 @@
+package mil.nga.giat.mage.map.cache;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import mil.nga.giat.mage.R;
+
+/**
+ * GeoPackage file cache overlay
+ */
+public class GeoPackageCacheOverlay extends CacheOverlay {
+
+    /**
+     * Mapping between table cache names and the table cache overlays
+     */
+    private Map<String, CacheOverlay> tables = new LinkedHashMap<String, CacheOverlay>();
+
+    /**
+     * Constructor
+     *
+     * @param name   GeoPackage name
+     * @param tables tables
+     */
+    public GeoPackageCacheOverlay(String name, List<CacheOverlay> tables) {
+        super(name, CacheOverlayType.GEOPACKAGE, true);
+
+        for (CacheOverlay table : tables) {
+            this.tables.put(table.getCacheName(), table);
+        }
+    }
+
+    public Integer getIconImageResourceId() {
+        return R.drawable.ic_geopackage;
+    }
+
+    public List<CacheOverlay> getChildren() {
+        List<CacheOverlay> children = new ArrayList<>();
+        children.addAll(tables.values());
+        return children;
+    }
+
+}
