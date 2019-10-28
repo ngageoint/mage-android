9601ed4d76d8e6afa8c435f29eb52ae2339d506b
Kevin Gilland <kgilland@kevins-mbp.lan>
2019-10-28 11:05:20 -0600

    Adding capability to do a fetch then a load, vs just fetching and loading all layers at the same time

--- java/mil/nga/giat/mage/sdk/fetch/StaticFeatureServerFetch.java
+++ java/mil/nga/giat/mage/sdk/fetch/StaticFeatureServerFetch.java
@@ -12,6 +12,7 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import mil.nga.giat.mage.sdk.connectivity.ConnectivityUtility;
 import mil.nga.giat.mage.sdk.datastore.DaoStore;
@@ -48,9 +49,6 @@ public class StaticFeatureServerFetch extends AbstractServerFetch {
 	// TODO test that icons are pulled correctly
 	public void fetch(boolean deleteLocal, OnStaticLayersListener listener) {
 
-		StaticFeatureHelper staticFeatureHelper = StaticFeatureHelper.getInstance(mContext);
-		LayerHelper layerHelper = LayerHelper.getInstance(mContext);
-
 		// if you are disconnect, skip this
 		if(!ConnectivityUtility.isOnline(mContext) || LoginTaskFactory.getInstance(mContext).isLocalLogin()) {
 			Log.d(LOG_NAME, "Disconnected, not pulling static layers.");
@@ -60,18 +58,20 @@ public class StaticFeatureServerFetch extends AbstractServerFetch {
 		Event event = EventHelper.getInstance(mContext).getCurrentEvent();
 		Log.d(LOG_NAME, "Pulling static layers for event " + event.getName());
 		try {
-			Collection<Layer> layers = layerResource.getLayers(event);
+			LayerHelper layerHelper = LayerHelper.getInstance(mContext);
 
 			if (deleteLocal) {
 				layerHelper.deleteAll(FEATURE_TYPE);
 			}
 
+			Collection<Layer> remoteLayers = layerResource.getLayers(event);
+
 			// get local layers
 			Collection<Layer> localLayers = layerHelper.readAll(FEATURE_TYPE);
 
-			layers.removeAll(localLayers);
+			remoteLayers.removeAll(localLayers);
 
-			for (Layer layer : layers) {
+			for (Layer layer : remoteLayers) {
 				layerHelper.create(layer);
 			}
 
@@ -80,81 +80,89 @@ public class StaticFeatureServerFetch extends AbstractServerFetch {
 			if (listener != null) {
 				listener.onStaticLayersLoaded(newLayers);
 			}
+		} catch (Exception e) {
+			Log.e(LOG_NAME, "Problem creating layers.", e);
+		}
+	}
 
-			for (Layer layer : newLayers) {
-				if (isCanceled) {
-					break;
-				}
-				if (!layer.isLoaded()) {
-					try {
-						Log.i(LOG_NAME, "Loading static features for layer " + layer.getName() + ".");
+	public void load(OnStaticLayersListener listener, Layer layer) {
+		try {
+			if (!layer.isLoaded()) {
+				StaticFeatureHelper staticFeatureHelper = StaticFeatureHelper.getInstance(mContext);
+				try {
+					Log.i(LOG_NAME, "Loading static features for layer " + layer.getName() + ".");
 
-						Collection<StaticFeature> staticFeatures = layerResource.getFeatures(layer);
+					Collection<StaticFeature> staticFeatures = layerResource.getFeatures(layer);
 
-						// Pull down the icons
-						Collection<String> failedIconUrls = new ArrayList<>();
-						for (StaticFeature staticFeature : staticFeatures) {
-							StaticFeatureProperty property = staticFeature.getPropertiesMap().get("styleiconstyleiconhref");
-							if (property != null) {
-								String iconUrlString = property.getValue();
+					// Pull down the icons
+					Collection<String> failedIconUrls = new ArrayList<>();
+					for (StaticFeature staticFeature : staticFeatures) {
+						StaticFeatureProperty property = staticFeature.getPropertiesMap().get("styleiconstyleiconhref");
+						if (property != null) {
+							String iconUrlString = property.getValue();
 
-								if (failedIconUrls.contains(iconUrlString)) {
-									continue;
-								}
+							if (failedIconUrls.contains(iconUrlString)) {
+								continue;
+							}
 
-								if (iconUrlString != null) {
-									File iconFile = null;
-									try {
-										URL iconUrl = new URL(iconUrlString);
-										String filename = iconUrl.getFile();
-										// remove leading /
-										if (filename != null) {
-											filename = filename.trim();
-											while (filename.startsWith("/")) {
-												filename = filename.substring(1, filename.length());
-											}
+							if (iconUrlString != null) {
+								File iconFile = null;
+								try {
+									URL iconUrl = new URL(iconUrlString);
+									String filename = iconUrl.getFile();
+									// remove leading /
+									if (filename != null) {
+										filename = filename.trim();
+										while (filename.startsWith("/")) {
+											filename = filename.substring(1, filename.length());
 										}
+									}
 
-										iconFile = new File(mContext.getFilesDir() + "/icons/staticfeatures", filename);
-										if (!iconFile.exists()) {
-											iconFile.getParentFile().mkdirs();
-											iconFile.createNewFile();
-											InputStream inputStream = layerResource.getFeatureIcon(iconUrlString);
-											if (inputStream != null) {
-												ByteStreams.copy(inputStream, new FileOutputStream(iconFile));
-												staticFeature.setLocalPath(iconFile.getAbsolutePath());
-											}
-										} else {
+									iconFile = new File(mContext.getFilesDir() + "/icons/staticfeatures", filename);
+									if (!iconFile.exists()) {
+										iconFile.getParentFile().mkdirs();
+										iconFile.createNewFile();
+										InputStream inputStream = layerResource.getFeatureIcon(iconUrlString);
+										if (inputStream != null) {
+											ByteStreams.copy(inputStream, new FileOutputStream(iconFile));
 											staticFeature.setLocalPath(iconFile.getAbsolutePath());
 										}
-									} catch (Exception e) {
-										// this block should never flow exceptions up! Log for now.
-										Log.w(LOG_NAME, "Could not get icon.", e);
-										failedIconUrls.add(iconUrlString);
-										if (iconFile != null && iconFile.exists()) {
-											iconFile.delete();
-										}
+									} else {
+										staticFeature.setLocalPath(iconFile.getAbsolutePath());
+									}
+								} catch (Exception e) {
+									// this block should never flow exceptions up! Log for now.
+									Log.w(LOG_NAME, "Could not get icon.", e);
+									failedIconUrls.add(iconUrlString);
+									if (iconFile != null && iconFile.exists()) {
+										iconFile.delete();
 									}
 								}
 							}
 						}
-
-						layer = staticFeatureHelper.createAll(staticFeatures, layer);
-						try {
-							DaoStore.getInstance(mContext).getLayerDao().update(layer);
-						} catch (SQLException e) {
-							throw new StaticFeatureException("Unable to update the layer to loaded: " + layer.getName());
-						}
-
-						Log.i(LOG_NAME, "Loaded static features for layer " + layer.getName());
-
-					} catch (StaticFeatureException e) {
-						Log.e(LOG_NAME, "Problem creating static features.", e);
 					}
+
+					layer = staticFeatureHelper.createAll(staticFeatures, layer);
+					try {
+						DaoStore.getInstance(mContext).getLayerDao().update(layer);
+					} catch (SQLException e) {
+						throw new StaticFeatureException("Unable to update the layer to loaded: " + layer.getName());
+					}
+
+					Log.i(LOG_NAME, "Loaded static features for layer " + layer.getName());
+
+					if(listener != null){
+						List<Layer> layers = new ArrayList<>(1);
+						layers.add(layer);
+						listener.onStaticLayersLoaded(layers);
+					}
+
+				} catch (StaticFeatureException e) {
+					Log.e(LOG_NAME, "Problem creating static features.", e);
 				}
 			}
 		} catch (Exception e) {
-			Log.e(LOG_NAME, "Problem creating layers.", e);
+			Log.e(LOG_NAME, "Problem loading layers.", e);
 		}
 	}
 
