e2ab9afb044b9252dbc00c00d3a050a3e46df141
Kevin Gilland <kgilland@kevins-mbp.lan>
2019-11-22 09:04:55 -0700

    Adding lower level testing for offline adapter

--- mage/tests/src/mil/nga/giat/mage/map/preference/OfflineLayersAdapterTest.java
+++ mage/tests/src/mil/nga/giat/mage/map/preference/OfflineLayersAdapterTest.java
@@ -10,6 +10,13 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import mil.nga.giat.mage.map.cache.CacheOverlay;
+import mil.nga.giat.mage.map.cache.CacheOverlayType;
+import mil.nga.giat.mage.sdk.datastore.layer.Layer;
+
 import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 
 /**
@@ -21,7 +28,7 @@ import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 public class OfflineLayersAdapterTest {
 
     @Test
-    public void testAdapterInit() {
+    public void testInit() {
         Context context = getApplicationContext();
         Assert.assertNotNull(context);
 
@@ -35,10 +42,52 @@ public class OfflineLayersAdapterTest {
     }
 
     @Test
-    public void testAdapter() {
+    public void testGroups() {
         Context context = getApplicationContext();
         Assert.assertNotNull(context);
 
         OfflineLayersAdapter adapter = new OfflineLayersAdapter(context, null);
+
+        CacheOverlay first = new CacheOverlay("first", CacheOverlayType.STATIC_FEATURE, false) {
+            @Override
+            public void removeFromMap() {
+
+            }
+        };
+        CacheOverlay child = new CacheOverlay("child", CacheOverlayType.XYZ_DIRECTORY, false) {
+            @Override
+            public void removeFromMap() {
+
+            }
+        };
+        CacheOverlay second = new CacheOverlay("second", CacheOverlayType.XYZ_DIRECTORY, true) {
+
+            private List<CacheOverlay> children = new ArrayList<>();
+
+            @Override
+            public void removeFromMap() {
+
+            }
+
+            public List<CacheOverlay> getChildren() {
+                return  children;
+            }
+        };
+        second.getChildren().add(child);
+        Layer third = new Layer();
+        third.setType("test");
+
+        adapter.getOverlays().add(first);
+        adapter.getSideloadedOverlays().add(second);
+        adapter.getDownloadableLayers().add(third);
+
+        Assert.assertEquals(3, adapter.getGroupCount());
+        Assert.assertEquals(first, adapter.getGroup(0));
+        Assert.assertEquals(second, adapter.getGroup(1));
+        Assert.assertEquals(third, adapter.getGroup(2));
+
+        Assert.assertEquals(1, adapter.getChildrenCount(1));
+        Assert.assertEquals(child, adapter.getChild(1, 0));
+
     }
 }
