f131430692a344d4f64371408357487368cec925
Kevin Gilland <kgilland@kevins-mbp.lan>
2019-11-22 10:51:23 -0700

    Adding additional tests

--- mage/tests/src/mil/nga/giat/mage/map/preference/OfflineLayersPreferenceActivityTest.java
+++ mage/tests/src/mil/nga/giat/mage/map/preference/OfflineLayersPreferenceActivityTest.java
@@ -1,11 +1,12 @@
 package mil.nga.giat.mage.map.preference;
 
-import android.content.Context;
+import android.Manifest;
 
 import androidx.lifecycle.Lifecycle;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import androidx.test.rule.GrantPermissionRule;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -14,7 +15,6 @@ import org.junit.runner.RunWith;
 
 import mil.nga.giat.mage.R;
 
-import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -23,6 +23,10 @@ import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.not;
 
+/**
+ * @see OnlineLayersPreferenceActivityTest
+ * @since 11/22/19
+ */
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class OfflineLayersPreferenceActivityTest {
@@ -30,6 +34,10 @@ public class OfflineLayersPreferenceActivityTest {
     @Rule
     public ActivityScenarioRule<TileOverlayPreferenceActivity> activityRule =
             new ActivityScenarioRule(TileOverlayPreferenceActivity.class);
+
+    @Rule
+    public GrantPermissionRule permissionRule =
+            GrantPermissionRule.grant(Manifest.permission.READ_EXTERNAL_STORAGE);
 
     @Before
     public void before(){
@@ -42,11 +50,10 @@ public class OfflineLayersPreferenceActivityTest {
         Thread.sleep(5000);
         onView(withId(R.id.tile_overlay_refresh)).check(matches(isEnabled()));
 
-        Context context = getApplicationContext();
-
         onView(withId(R.id.tile_overlay_refresh)).perform(click());
         onView(withId(R.id.tile_overlay_refresh)).check(matches(isEnabled()));
-        onView(withId(R.id.downloadable_layers_content)).check(matches(isDisplayed()));
 
+        onView(withId(R.id.downloadable_layers_content)).check(matches(isDisplayed()));
+        onView(withId(R.id.downloadable_layers_no_content)).check(matches(not(isDisplayed())));
     }
 }
