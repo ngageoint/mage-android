d1a48046397710a73de2d2517eac49698c1dd38a
Billy Newman <newmanw10@gmail.com>
2020-01-24 14:45:10 -0700

    add primary and secondary feed field getters (#22)
    Co-authored-by: Dan <daniel.barela@gmail.com>

--- java/mil/nga/giat/mage/sdk/datastore/observation/Observation.java
+++ java/mil/nga/giat/mage/sdk/datastore/observation/Observation.java
@@ -379,6 +379,14 @@ public class Observation implements Comparable<Observation>, Temporal {
         return getField("variantField");
     }
 
+    public ObservationProperty getPrimaryFeedField() {
+        return getField("primaryFeedField");
+    }
+
+    public ObservationProperty getSecondaryFeedField() {
+        return getField("secondaryFeedField");
+    }
+
     public ObservationProperty getField(String name) {
         ObservationProperty field = null;
         Collection<ObservationForm> forms = getForms();
