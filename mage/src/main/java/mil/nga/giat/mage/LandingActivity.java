3f8f2917b7f774afbc7f99312c0bfdb0d53613f5
William Newman <newmanw10@gmail.com>
2022-04-01 09:47:02 -0600

    Remove custom button no shadow style

--- mage/src/main/java/mil/nga/giat/mage/LandingActivity.java
+++ mage/src/main/java/mil/nga/giat/mage/LandingActivity.java
@@ -23,6 +23,7 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
