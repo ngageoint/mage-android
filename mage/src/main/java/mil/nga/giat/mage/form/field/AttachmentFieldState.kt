0ece693fb5b34c24417a4e4b5f04e6596403b8bd
William Newman <newmanw10@gmail.com>
2022-04-28 07:57:10 -0600

    Fix attachment validation check if min is zero

--- mage/src/main/java/mil/nga/giat/mage/form/field/AttachmentFieldState.kt
+++ mage/src/main/java/mil/nga/giat/mage/form/field/AttachmentFieldState.kt
@@ -14,23 +14,24 @@ class AttachmentFieldState(definition: FormField<Attachment>) :
   )
 
 private fun errorMessage(definition: FormField<Attachment>, value: FieldValue.Attachment?): String {
-  val size = value?.attachments?.filter { it.action != Media.ATTACHMENT_DELETE_ACTION }?.size
+  val size = value?.attachments?.filter { it.action != Media.ATTACHMENT_DELETE_ACTION }?.size ?: 0
 
   val fieldDefinition = definition as? AttachmentFormField
-  return if (fieldDefinition?.min != null && (size == null || size < fieldDefinition.min.toDouble())) {
+  return if (fieldDefinition?.min != null && size < fieldDefinition.min.toDouble()) {
     return "Must include at least  ${fieldDefinition.min} ${if (fieldDefinition.min.toInt() > 1) "attachments" else "attachment"}"
-  } else if (fieldDefinition?.max != null && (size != null && size > fieldDefinition.max.toInt())) {
+  } else if (fieldDefinition?.max != null && size > fieldDefinition.max.toInt()) {
     return "Cannot include more than ${fieldDefinition.max} ${if (fieldDefinition.max.toInt() > 1) "attachments" else "attachment"}"
   } else ""
 }
 
 private fun isValid(definition: FormField<Attachment>, value: FieldValue.Attachment?): Boolean {
-  val size = value?.attachments?.filter { it.action != Media.ATTACHMENT_DELETE_ACTION }?.size
+  val size = value?.attachments?.filter { it.action != Media.ATTACHMENT_DELETE_ACTION }?.size ?: 0
 
   val fieldDefinition = definition as? AttachmentFormField
-  return if (fieldDefinition?.min != null && (size == null || size < fieldDefinition.min.toInt())) {
+
+  return if (fieldDefinition?.min != null && size < fieldDefinition.min.toInt()) {
     false
-  } else !(fieldDefinition?.max != null && (size != null && size > fieldDefinition.max.toInt()))
+  } else !(fieldDefinition?.max != null && size > fieldDefinition.max.toInt())
 }
 
 private fun hasValue(value: FieldValue.Attachment?): Boolean {
