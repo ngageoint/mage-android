f0f8c04259a9f688c2d9437abf98b044700a5891
Nathan Russell <nathan.russell@omnifederal.com>
2026-02-04 08:54:44 -0600

    [mage-1760] (#105)
    Add logic to retrieve event forms when observations are fetched from the server

--- mage/src/main/java/mil/nga/giat/mage/network/event/EventService.kt
+++ mage/src/main/java/mil/nga/giat/mage/network/event/EventService.kt
@@ -3,8 +3,12 @@ package mil.nga.giat.mage.network.event
 import mil.nga.giat.mage.database.model.event.Event
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface EventService {
    @GET("/api/events")
    suspend fun getEvents(): Response<List<Event>>
+
+   @GET("/api/events/{eventId}")
+   suspend fun getEvent(@Path("eventId") eventId: String): Response<Event>
 }
